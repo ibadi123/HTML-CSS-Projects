b8b3ede912660e82a05799497f6630fb3c2ade9f
ibadi123 <ibadi12345@gmail.com>
2020-04-10 23:31:41 +0500

    Added & 10-4-20

---
+++ LittleTrick/app.js
@@ -0,0 +1,20 @@
+function xyz() {
+  const myImg = document.createElement("img");
+
+  myImg.src = "https://source.unsplash.com/random";
+
+  myImg.style.width = "400px";
+
+  document.body.append(myImg);
+  myImg.style.transition = "all 2s";
+
+  setInterval(() => {
+    const width = Math.floor(document.body.clientWidth * Math.random());
+    const height = Math.floor(document.body.clientHeight * Math.random());
+    myImg.style.transform = `translate(${width}px,${height}px)`;
+  }, 2000);
+}
+
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', xyz);
